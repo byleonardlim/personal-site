656a5d9ef46fd578d6832c19188f4cb87b98496a
Leonard Lim <me@byleonardlim.com>
2025-09-28 23:06:15 +0800

    removed tracking for .next folder

--- .next/static/chunks/src_app_layout_tsx_e0c3d4b4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d85e1fa8._.css",
-  "static/chunks/32a9e_next_dist_8bfaaeec._.js"
-],
-    source: "dynamic"
-});
